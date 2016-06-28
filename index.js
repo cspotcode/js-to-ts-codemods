b3a4375d70a1c121fe75448dc61174f1141156cf
Andrew Bradley <cspotcode@gmail.com>
2016-06-28 01:50:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// Load sourcemap support
+require('source-map-support').install();
+module.exports = require('./bin/index');
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// Gotta reference the typings at least once.
+/// <reference path="typings/index.d.ts" />
